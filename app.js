f8bd8566faa43ed90a999512928dda2a28372843
Satyam204 <satyamshivam366@gmail.com>
2023-07-08 01:38:41 +0530

    the blog project started

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express=require('express');
+
+const app=express();
+const PORT=process.env.PORT||5000;
+
+app.get('',(req,res)=>
+{
+   res.send("hello");
+});
+
+app.listen(PORT,()=>
+{
+    console.log(`running on port  ${PORT}`);
+})
