d6f9e49ca3e07009919bff810df3903a94c83f97
Satyam204 <satyamshivam366@gmail.com>
2023-07-10 01:08:15 +0530

    Css and Database connection Completed

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 
 const express=require('express');
 const expressLayout = require('express-ejs-layouts');
-
+const connectDB=require('./backend/server/db')
 
 const app=express();
 const PORT=process.env.PORT||5000;
+
+connectDB();
 
 app.use(express.static('public'));
 
